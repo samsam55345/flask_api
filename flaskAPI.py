6818c2c23924075672b0f201197d4031a13419ec
Sami Frank <sami.frank@merrillcorp.com>
2020-06-05 20:50:57 -0500

    beginning work on flask

--- flaskAPI.py
+++ flaskAPI.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+
 
 app = Flask(__name__)
-
-
+page_count = 0
 
 @app.route("/")
 def welcome():
-    return "Welcome to my exploration into building REST APIs!"
+    global page_count
+    page_count += 1
+    return render_template("welcome.html",
+    message = "current number of refreshes: %d" % page_count)
+
+@app.route("/date")
+def date():
+    now = datetime.now()
+    return "The date is %s-%s-%s" % (now.month, now.day, now.year)
+
+@app.route("/sadday")
+def sad():
+    return "When it rains, it pours :("
